0a71eb600eaa78e23a63ae3526d6a06833692882
StephhyL <Stephanie Li>
2022-01-02 12:46:03 -0500

    initalize players and some methods to test

--- game.rb
+++ game.rb
@@ -1,1 +1,17 @@
 require_relative './player'
+
+class Game
+  def initialize
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+  end
+
+  def play
+    puts '----- NEW TURN ----'
+  end
+
+  def info
+    p @player1
+    p @player2
+  end
+end
