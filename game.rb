d0da2d9ebcdc4477154bfc64b725a95b20bfc8e4
StephhyL <Stephanie Li>
2022-01-02 13:13:09 -0500

    comments to organize thoughts and add players array

--- game.rb
+++ game.rb
@@ -4,17 +4,28 @@ class Game
   def initialize
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
+    @players = [@player1, @player2]
+  end
+
+  def goodbye_message
+    puts '---- GAME OVER -----'
+    puts 'Good bye!'
   end
 
   def play
-    puts "#{@player1}: What does 5 plus 3 equal?"
-    print '> '
-    answer = gets.chomp.to_i
+    # intro
+    puts 'Welcome to Two-O-Player Math Game!'
 
-    puts answer === 8 ? 'YES! You are correct' : 'Seriously? No!'
+    # player question after each round
+    @player1.answers_question
 
-    # puts "> #{gets.chomp}"
+    # score display after each round
+    puts "P1: #{@player1.lives_left} vs P2: #{@player2.lives_left}"
+
     puts '----- NEW TURN ----'
+
+    #closing remarks
+    goodbye_message
   end
 
   def info
