1f60bcb17d942f80fe3b7461541719f353243838
StephhyL <Stephanie Li>
2022-01-02 13:08:39 -0500

    add logic for when actual answer equals expected answer

--- player.rb
+++ player.rb
@@ -1,7 +1,31 @@
 class Player
+  attr_reader :name
+  attr_accessor :lives
+
   def initialize(name)
     @name = name
     @lives = 3
-    @score = 0
+  end
+
+  def answers_question
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    puts "#{self.name}: What does #{num1} plus #{num2} equal?"
+    print '> '
+    actual_answer = gets.chomp.to_i
+    expected_answer = num1 + num2
+
+    puts(
+      if actual_answer === expected_answer
+        'YES! You are correct'
+      else
+        self.lives -= 1
+        'Seriously? No!'
+      end,
+    )
+  end
+
+  def lives_left
+    "#{@lives}/3"
   end
 end
